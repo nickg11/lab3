a0de75e7407e3dc9c5c65928dc8fbe51a1c8f1ed
Nicholas Gingras <nscgingras@gmail.com>
2021-10-12 20:45:57 -0400

    Add project files.

---
+++ lab3/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lab3.models;
+
+namespace lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Razor() => View();
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            ViewData["bottles"] = Request.Form["bottles"];
+            return View();
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        public IActionResult DisplayPerson()
+        {
+            ViewData["first"] = Request.Form["first"];
+            ViewData["last"] = Request.Form["last"];
+            ViewData["email"] = Request.Form["email"];
+            ViewData["age"] = Request.Form["age"];
+            ViewData["DoB"] = Request.Form["DoB"];
+            ViewData["password"] = Request.Form["password"];
+            ViewData["description"] = Request.Form["description"];
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ lab3/models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab3.models
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public string email;
+        public DateTime DoB;
+        public string password;
+        public string description;
+    }
+}
